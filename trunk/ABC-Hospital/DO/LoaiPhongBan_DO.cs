b847b117167a5f46ebd8481dc2d89ee3baa928de
hungb92@gmail.com <hungb92@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-03 15:18:21 +0000

    Coding LoaiPhongBan_DO

---
+++ trunk/ABC-Hospital/DO/LoaiPhongBan_DO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+    public class LoaiPhongBan_DO
+    {
+        public string _MaLoaiPhongBan { get; set; }
+        public string _TenLoaiPhongBan { get; set; }
+        public string _MoTa { get; set; }
+        public bool? _TrangThai { get; set; }
+    }
+}
