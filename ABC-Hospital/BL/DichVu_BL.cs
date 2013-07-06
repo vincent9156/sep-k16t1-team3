cacb2b849e3c168115b5eda16765b93037260846
Manchester.United.K16T1@gmail.com <Manchester.United.K16T1@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-06 03:51:36 +0000

    Coding phan LoadDanhSachDIchVu, TimKiemTheoMa, TimKiemTheoTen

---
+++ ABC-Hospital/BL/DichVu_BL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DA;
+using DO;
+namespace BL
+{
+    public class DichVu_BL
+    {
+        public static List<DichVu_DO> LoadDanhSachDichVu()
+        {
+            return DA.DichVu_DA.LoadDanhSachDichVu();
+        }
+        public static List<DichVu_DO> TimKiemTheoMa(string madichvu)
+        {
+            return DA.DichVu_DA.TimKiemTheoMa(madichvu);
+        }
+        public static List<DichVu_DO> TimKiemTheoTen(string tenloai)
+        {
+            return DA.DichVu_DA.TimKiemTheoTen(tenloai);
+        }
+    }
+}
