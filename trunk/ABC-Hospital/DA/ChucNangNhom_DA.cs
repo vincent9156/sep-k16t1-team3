4de726dc1d638af586dc5872ff75c05b29d93c7b
khanghuynh92 <khanghuynh92@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-06 01:43:47 +0000

    Update thieu xot

---
+++ trunk/ABC-Hospital/DA/ChucNangNhom_DA.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+
+namespace DA
+{
+    public class ChucNangNhom_DA
+    {
+        // Lấy danh sách phân quyền của nhóm từ csdl
+        public static List<ChucNangNhom_DO> LayChucNangNhom(string manhom)
+        {
+            List<ChucNangNhom_DO> dsChucnangnhom = new List<ChucNangNhom_DO>();
+            using (Entities tk = new Entities())
+            {
+                var query = from u in tk.ChucNangNhoms
+                            where (u.MaNhom == manhom)
+                            select u;
+                foreach (var row in query)
+                {
+                    ChucNangNhom_DO us = new ChucNangNhom_DO();
+                    us._MaChucNang = row.MaChucNang;
+                    us._MaNhom = row.MaNhom;
+                    us._TrangThai = row.TrangThai;
+                    dsChucnangnhom.Add(us);
+                }
+            }
+            return dsChucnangnhom;
+        }
+        // Thêm quyền cho nhóm
+        public static void ThemQuyenChoNhom(int machucnang, string manhom, bool trangthai)
+        {
+            using (Entities tk = new Entities())
+            {
+                ChucNangNhom us = new ChucNangNhom();
+                us.MaChucNang = machucnang;
+                us.MaNhom = manhom;
+                us.TrangThai = trangthai;
+                tk.AddToChucNangNhoms(us);
+                tk.SaveChanges();
+            }
+        }
+        // Cập nhật quyền cho nhóm
+        public static void CapNhatQuyenChoNhom(int machucnang, string manhom, bool trangthai)
+        {
+            using (Entities tk = new Entities())
+            {
+                var query = (from u in tk.ChucNangNhoms
+                             where u.MaChucNang == machucnang
+                             select u).First();
+                query.MaChucNang = machucnang;
+                query.MaNhom = manhom;
+                query.TrangThai = trangthai;
+                tk.SaveChanges();
+            }
+        }
+    }
+}
---
+++ trunk/ABC-Hospital/DA/ChucNang_DA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+namespace DA
+{
+    public class ChucNang_DA
+    {
+        // Hàm lấy danh sách chức năng từ dưới csdl
+        public static List<ChucNang_DO> LayChucNang()
+        {
+            List<ChucNang_DO> dsChucnang = new List<ChucNang_DO>();
+            using (Entities tk = new Entities())
+            {
+                var query = from u in tk.ChucNangs
+                            select u;
+
+                foreach (var row in query)
+                {
+                    ChucNang_DO us = new ChucNang_DO();
+                    us._MaChucNang = row.MaChucNang;
+                    us._TenChucNang = row.TenChucNang;
+                    dsChucnang.Add(us);
+                }
+            }
+            return dsChucnang;
+        }
+    }
+}
