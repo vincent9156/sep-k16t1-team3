a84c623ce65fc0eb6cba3f29efcbcf5b1d3e9332
dangduongcnt@gmail.com <dangduongcnt@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-06 04:19:08 +0000

    NhomDichVu_DO danglam

---
+++ trunk/ABC-Hospital/DO/NhomDichVu_DO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+
+    public class NhomDichVu_DO
+    {
+        public String _MaNhomDichVu { get; set; }
+        public String _TenNhomDichVu { get; set; }
+        public String _MoTa { get; set; }
+        public bool? _TrangThai { get; set; }
+    }
+}
