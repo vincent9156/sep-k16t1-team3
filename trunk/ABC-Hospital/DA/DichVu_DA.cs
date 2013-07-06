ed30d9b5ba3bc5bc2932f0a16254273627b23377
Manchester.United.K16T1@gmail.com <Manchester.United.K16T1@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-06 03:53:02 +0000

    Coding phan loaddanhsachdichvu, timkiemtheoma, timkiemtheoten

---
+++ trunk/ABC-Hospital/DA/DichVu_DA.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+
+namespace DA
+{
+    public class DichVu_DA
+    {
+        /// <summary>
+        /// Hàm lấy danh sách dịch vụ từ Database
+        /// </summary>
+        /// <returns></returns>
+        public static List<DichVu_DO> LoadDanhSachDichVu()
+        {
+        List<DichVu_DO> dsDichVu = new List<DichVu_DO>();
+        using (Entities mh = new Entities())
+        {
+            var query = from u in mh.DichVus select u;
+            foreach (var row in query)
+            {
+                DichVu_DO us = new DichVu_DO();
+                us._MaDichVu = row.MaDichVu;
+                us._MaNhomDichVu = row.MaNhomDichVu;
+                us._TenDichVu = row.TenDichVu;
+                us._DonGia = row.DonGia;
+                us._MoTa = row.MoTa;
+                us._TrangThai = row.TrangThai;
+                dsDichVu.Add(us);
+            }
+        }
+        return dsDichVu;
+        }
+        /// <summary>
+        /// Tìm kiếm theo mã dịch vụ
+        /// </summary>
+        /// <returns></returns>
+        public static List<DichVu_DO> TimKiemTheoMa(string madichvu)
+        {
+            List<DichVu_DO> dsDichVu = new List<DichVu_DO>();
+            using (Entities mh = new Entities())
+            {
+                var query = from u in mh.DichVus
+                            where u.MaDichVu.ToLower().Contains(madichvu.ToLower())
+                            select u;
+                foreach (var row in query)
+                {
+                    DichVu_DO us = new DichVu_DO();
+                    us._MaDichVu = row.MaDichVu;
+                    us._MaNhomDichVu = row.MaNhomDichVu;
+                    us._TenDichVu = row.TenDichVu;
+                    us._DonGia = row.DonGia;
+                    us._MoTa = row.MoTa;
+                    us._TrangThai = row.TrangThai;
+                    dsDichVu.Add(us);
+                }
+             }
+            return dsDichVu;
+        }
+        /// <summary>
+        /// Tìm kiếm theo tên
+        /// </summary>
+        /// <returns></returns>
+        public static List<DichVu_DO> TimKiemTheoTen(string tenloai)
+        {
+            List<DichVu_DO> dsDichVu = new List<DichVu_DO>();
+            using (Entities mh = new Entities())
+            {
+                var query = from u in mh.DichVus
+                            where u.TenDichVu.ToLower().Contains(tenloai.ToLower())
+                            select u;
+                foreach (var row in query)
+                {
+                    DichVu_DO us = new DichVu_DO();
+                    us._MaDichVu = row.MaDichVu;
+                    us._MaNhomDichVu = row.MaNhomDichVu;
+                    us._TenDichVu = row.TenDichVu;
+                    us._DonGia = row.DonGia;
+                    us._MoTa = row.MoTa;
+                    us._TrangThai = row.TrangThai;
+                    dsDichVu.Add(us);
+                }
+            }
+            return dsDichVu;
+        }
+    }
+}
