cad0c4e7d44ce59108899fe90cdcbdc6a89fb932
hungb92@gmail.com <hungb92@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-06 02:08:24 +0000

    Coding PhongBan_DO

---
+++ ABC-Hospital/DO/PhongBan_DO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+    public class PhongBan_DO
+    {
+        public string _MaPhongBan { get; set; }
+        public string _TenPhongBan { get; set; }
+        public string _LoaiPhongBan { get; set; }
+        public string _NhomDichVu { get; set; }
+        public int? _SoLuongPhong { get; set; }
+        public string _MoTa { get; set; } 
+        public bool? _TrangThai { get; set; }
+    }
+}
