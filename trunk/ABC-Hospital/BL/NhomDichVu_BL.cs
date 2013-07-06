7fe76903d944eb19f1130c9d4e4b5f9f0119bfb3
dangduongcnt@gmail.com <dangduongcnt@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-06 04:17:54 +0000

    nhomdichvu_BL danglam

---
+++ trunk/ABC-Hospital/BL/NhomDichVu_BL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+using DA;
+
+namespace BL
+{
+    public class NhomDichVu_BL
+    {
+        //lay danh sach nhom dich vu tu DA sang BL
+        public static List<NhomDichVu_DO> LayDanhSachNhomDichVu()
+        {
+            return DA.NhomDichVu_DA.LayNhomDIchVu();
+        }
+         // them 1 Nhom DIch Vu vao danh dach nhom dich vu
+        public static void ThemNhomDichVu(string MaNhom, string TenNhom, string Mota, bool TrangThai)
+        {
+            DA.NhomDichVu_DA.ThemNhomDichVu(MaNhom, TenNhom, Mota, TrangThai);
+        }
+        // cap nhat nhom dich vu
+        public static void CapNhatNhomDichVu(string MaNhom, string TenNhom, string Mota, bool TrangThai)
+        {
+            DA.NhomDichVu_DA.CapNhatNhomDichVu(MaNhom, TenNhom, Mota, TrangThai);
+        }
+    }
+        
+}
