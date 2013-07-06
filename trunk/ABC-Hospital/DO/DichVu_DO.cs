93602070459c6ce283de5b20a7d72a3f2b9bd040
Manchester.United.K16T1@gmail.com <Manchester.United.K16T1@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-06 03:54:06 +0000

    Coding phan loaddanhsachdichvu, timkiemtheoma, timkiemtheoten

---
+++ trunk/ABC-Hospital/DO/DichVu_DO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+    public class DichVu_DO
+    {
+        public string _MaDichVu { get; set;}
+        public string _MaNhomDichVu { get; set; }
+        public string _TenDichVu { get; set; }
+        public int? _DonGia { get; set; }
+        public string _MoTa { get; set; }
+        public bool? _TrangThai { get; set; }
+    }
+}
