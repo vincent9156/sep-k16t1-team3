9418c91d3f6032367c5dc1d456a1e01385440e8d
Manchester.United.K16T1@gmail.com <Manchester.United.K16T1@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-07 17:18:08 +0000

    Coding phan Dich Vu: them moi dich vu, cap nhat dich vu

--- trunk/ABC-Hospital/BL/DichVu_BL.cs
+++ trunk/ABC-Hospital/BL/DichVu_BL.cs
@@ -8,17 +8,32 @@ namespace BL
 {
     public class DichVu_BL
     {
+            // Hàm load danh sáhc dịch vụ
         public static List<DichVu_DO> LoadDanhSachDichVu()
         {
             return DA.DichVu_DA.LoadDanhSachDichVu();
         }
+            // Hàm tìm kiếm theo theo mã dịch vụ
         public static List<DichVu_DO> TimKiemTheoMa(string madichvu)
         {
             return DA.DichVu_DA.TimKiemTheoMa(madichvu);
         }
+            // Hàm tìm kiếm theo tên dịch vụ
         public static List<DichVu_DO> TimKiemTheoTen(string tenloai)
         {
             return DA.DichVu_DA.TimKiemTheoTen(tenloai);
+        }
+            // Hàm thêm mới một dịch vụ
+        public static void ThemDichVu(string maso, string manhomdichvu, string tendichvu, int dongia, string mota, bool trangthai)
+        {
+            DA.DichVu_DA.ThemDichVu(maso, manhomdichvu, tendichvu, dongia, mota, trangthai);
+            
+        }
+            // Hàm cập nhật dịch vụ
+        public static void CapNhatDichVu(string maso, string manhomdichvu, string tendichvu, int dongia, string mota, bool trangthai)
+        {
+            DA.DichVu_DA.CapNhatDichVu(maso, manhomdichvu, tendichvu, dongia, mota, trangthai);
+
         }
     }
 }
