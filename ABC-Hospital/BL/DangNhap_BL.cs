3248596241f468886b7eb88773e177484f95a644
khanghuynh92 <khanghuynh92@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-08 20:19:39 +0000

    add thêm class DangNhap_BL để quản lý việc đăng nhập của user - 09/07

---
+++ ABC-Hospital/BL/DangNhap_BL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DA;
+using DO;
+using System.Text.RegularExpressions;
+namespace BL
+{
+    public class DangNhap_BL
+    {
+        // Hàm kiểm tính hợp lệ khi người dùng nhập vào 
+        public static bool KiemTraHopLe(string username, string password)
+        {
+            if (Regex.IsMatch(username, "^[a-zA-Z0-9]{1,20}$")
+             && Regex.IsMatch(password, "^[a-zA-Z0-9]{6,50}$"))
+            {
+                return true;
+            }
+            return false;
+        }
+        // Hàm kiểm tra tính đúng đắn của tài khoản
+        public static bool KiemTraDungTK(string user, string pass)
+        {
+            List<NguoiDung_DO> ds = BL.NguoiDung_BL.LayNguoiDung();
+            foreach (NguoiDung_DO line in ds)
+            {
+                if (line._MaNguoiDung == user && line._MatKhau == pass)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        // Lấy người dùng trong danh sách người dùng để kiểm tra user vs pass
+        public static List<NguoiDung_DO> LayNguoiDung()
+        {
+            return DA.DangNhap_DA.LayNguoiDung();
+        }
+        // Lấy quyền theo nhóm ( đăng nhập theo tài khoản)
+        public static List<ChucNangNhom_DO> LayQuyen(string manhom)
+        {
+            return DA.DangNhap_DA.LayQuyen(manhom);
+        }
+    }
+}
