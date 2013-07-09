8ebe08159cc97b385c9c75276c2f6a5267f30211
hungb92@gmail.com <hungb92@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-09 09:51:47 +0000

    Hoan thanh ChiTietPhongBan_DO va DanhSachBan_DO

---
+++ trunk/ABC-Hospital/DO/ChiTietPhongBan_DO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+    public class ChiTietPhongBan_DO
+    {
+        public string _MaCTPB { get; set; }
+        public string _PhongSo { get; set; }
+        public string _MoTa { get; set; }
+        public bool? _TrangThai { get; set; }
+    }
+}
---
+++ trunk/ABC-Hospital/DO/DanhSachBan_DO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+    public class DanhSachBan_DO
+    {
+        public string _MaBan { get; set; }
+        public string _MoTa { get; set; }
+        public bool? _TrangThai { get; set; }
+    }
+}
