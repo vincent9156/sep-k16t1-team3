29b5a275fc2c095da9d4e18db01f7fcfeaea1037
hungb92@gmail.com <hungb92@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-09 09:49:59 +0000

    Hoan thanh ChiTietPhongBan_BL va DanhSachBan_BL

---
+++ ABC-Hospital/BL/ChiTietPhongBan_BL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+using DA;
+namespace BL
+{
+    public class ChiTietPhongBan_BL
+    {
+        // Hàm lấy chi tiết phòng ban
+        public static List<ChiTietPhongBan_DO> LayChiTietPhongBan(string maphong)
+        {
+            return DA.ChiTietPhongBan_DA.LayChiTietPhongBan(maphong);
+        }
+        // Hàm thêm chi tiet phòng ban
+        public static void ThemChiTietPhongBan(string mactpb, string phongso,string phongban, string mota, bool trangthai)
+        {
+            DA.ChiTietPhongBan_DA.ThemChiTietPhongBan(mactpb, phongso, phongban, mota, trangthai);
+        }
+        // Hàm cập nhật loại chi tiet phòng ban
+        public static void CapNhatChiTietPhongBan(string mactpb, string phongso, string mota, bool trangthai)
+        {
+            DA.ChiTietPhongBan_DA.CapNhatChiTietPhongBan(mactpb, phongso, mota, trangthai);
+        }
+        // Hàm lấy mã ctpb lớn nhất
+        public static String MaChiTietPhongBanLonNhat(string maphong)
+        {
+            return DA.ChiTietPhongBan_DA.MaChiTietPhongBanLonNhat(maphong);
+        }
+        // Hàm tìm kiếm theo mã
+        public static List<ChiTietPhongBan_DO> TimKiemTheoMa(string maso, string maphong)
+        {
+            return DA.ChiTietPhongBan_DA.TimKiemTheoMa(maso, maphong);
+        }
+        // Hàm tìm kiếm theo tên
+        public static List<ChiTietPhongBan_DO> TimKiemTheoTen(string tenphong, string maphong)
+        {
+            return DA.ChiTietPhongBan_DA.TimKiemTheoTen(tenphong, maphong);
+        }
+    }
+}
