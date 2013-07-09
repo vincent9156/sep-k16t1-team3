7427dcdd290ef593258b05a00136c9ada8349301
hungb92@gmail.com <hungb92@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-09 10:02:18 +0000

    Thay đổi bởi Minh Hòa

--- ABC-Hospital/GUI/ucDichvu.cs
+++ ABC-Hospital/GUI/ucDichvu.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -11,23 +11,201 @@ namespace GUI
 {
     public partial class ucDichvu : UserControl
     {
+        private bool add = false;
+        private bool sua = false;
         public ucDichvu()
         {
             InitializeComponent();
             lblDatetime.Text = BL.HamDungChung_BL.Updatetime();
             LoadDanhSachDichVu();
+            LoadDanhSachNhomDichVu();
         }
         private void LoadDanhSachDichVu()
         {
             _grdDichVu.DataSource = BL.DichVu_BL.LoadDanhSachDichVu();
+            // Enable/Disable cho các Button
+            _txtMaso.Enabled = false;
+            _btnHuy.Enabled = false;
+            _btnLuu.Enabled = false;
+            _btnSua.Enabled = true;
+            // Enable/Disable các giá trị
+            _txtTendichvu.Enabled = false;
+            _tbxMota.Enabled = false;
+            _chkTrangthai.Enabled = false;
+            _cboNhomdichvu.Enabled = false;
+            _txtDongia.Enabled = false;
         }
-
-        private void pnlTieude_Click(object sender, EventArgs e)
+        private void LoadDanhSachNhomDichVu()
         {
+            _cboNhomdichvu.DataSource = BL.NhomDichVu_BL.LayNhomDichVu();
+            _cboNhomdichvu.DisplayMember = "_TenNhom";
+            _cboNhomdichvu.ValueMember = "_MaNhom";
+            _cboNhomdichvu.Text = "";
+        }
+        private void MaTuDong()
+        {
+            string malonnhat = BL.DichVu_BL.LayMaDichVuLonNhat();
+            string maso = "";
+            if (malonnhat.Length == 0)
+            {
+                _txtMaso.Text = "DV001";
+            }
+            else
+            {
+                int masotam = Convert.ToInt32(malonnhat.Substring(2, malonnhat.Length - 2)) + 1;
+                for (int i = 0; i < 3 - masotam.ToString().Length; i++)
+                {
+                    maso += "0";
+                }
+                _txtMaso.Text = "DV" + maso + masotam;
+            }
+        }
+        private void ResetText()
+        {
+            _txtMaso.Text = "";
+            _txtTendichvu.Text = "";
+            _cboNhomdichvu.Text = "";
+            _txtDongia.Text = "";
+            _tbxMota.Text = "";
+            _chkTrangthai.Checked = false;
+        }
+        private bool KiemTraHopLe()
+        {
+            if (_txtTendichvu.Text == ""  || _cboNhomdichvu.Text == "")
+            {
+                return false;
+            }
+            return true;
+        }
+        private void _btnThem_Click(object sender, EventArgs e)
+        {
+            //button
+            ResetText();
+            MaTuDong();
+            _btnLuu.Enabled = true;
+            _btnSua.Enabled = false;
+            _btnThem.Enabled = false;
+            _btnHuy.Enabled = true;
+            add = true;
+            //gia tri
+            _txtTendichvu.Enabled = true;
+            _tbxMota.Enabled = true;
+            _chkTrangthai.Enabled = true;
+            _cboNhomdichvu.Enabled = true;
+            _txtDongia.Enabled = true;
+        }
+        private void _btnLuu_Click(object sender, EventArgs e)
+        {
+            if (add == true)//them
+            {
+                if (KiemTraHopLe() == true)
+                {
+                    try
+                    {
+                        BL.DichVu_BL.ThemDichVu(_txtMaso.Text, _cboNhomdichvu.SelectedValue.ToString(), _txtTendichvu.Text, _txtDongia.Text, _tbxMota.Text, _chkTrangthai.Checked);
+                        MessageBox.Show("Đã thêm dịch vụ " + _txtTendichvu.Text + " thành công!");
+                        LoadDanhSachDichVu();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Đã có lỗi xảy ra! Quá trình thêm thất bại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            else
+            {
+                if (sua == true)//sua
+                {
+                    if (KiemTraHopLe() == true)
+                    {
+                        try
+                        {
+                            BL.DichVu_BL.ThemDichVu(_txtMaso.Text, _cboNhomdichvu.SelectedValue.ToString(), _txtTendichvu.Text, _txtDongia.Text, _tbxMota.Text, _chkTrangthai.Checked);
+                            MessageBox.Show("Đã cập nhật dịch vụ " + _txtTendichvu.Text + " thành công!");
+                            LoadDanhSachDichVu();
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Đã có lỗi xảy ra! Quá trình sửa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+            }
+            _btnSua.Enabled = true;
+            _btnThem.Enabled = true;
+            _btnLuu.Enabled = false;
+            _btnHuy.Enabled = false;
+            _txtTendichvu.Enabled = false;
+            _txtDongia.Enabled = false;
+            _tbxMota.Enabled = false;
+            _chkTrangthai.Enabled = false;
+            _cboNhomdichvu.Enabled = false;
+            add = false;
+            sua = false;
+        }
+        private void _btnSua_Click(object sender, EventArgs e)
+        {
+            sua = true;
+            //button
+            _btnLuu.Enabled = true;
+            _btnThem.Enabled = false;
+            _btnSua.Enabled = false;
+            _btnHuy.Enabled = true;
+            //gia tri
+            _txtTendichvu.Enabled = true;
+            _tbxMota.Enabled = true;
+            _chkTrangthai.Enabled = true;
+            _txtDongia.Enabled = true;
+            _cboNhomdichvu.Enabled = true;
+        }
+        private void XemChiTietDichVu(int pos)
+        {
+            try
+            {
+                _txtMaso.Text = _grdDichVu.Rows[pos].Cells[0].Value.ToString().Trim();
+                _txtTendichvu.Text = _grdDichVu.Rows[pos].Cells[1].Value.ToString();
+                _cboNhomdichvu.Text = _grdDichVu.Rows[pos].Cells[2].Value.ToString();
+                _txtDongia.Text = _grdDichVu.Rows[pos].Cells[3].Value.ToString();
+                _tbxMota.Text = _grdDichVu.Rows[pos].Cells[4].Value.ToString();
+                if (_grdDichVu.Rows[pos].Cells[5].Value.ToString() == "True")
+                {
+                    _chkTrangthai.Checked = true;
+                }
+                else
+                {
+                    if (_grdDichVu.Rows[pos].Cells[5].Value.ToString() == "False")
+                    {
+                        _chkTrangthai.Checked = false;
+                    }
+                }
+            }
+            catch (Exception e)
+            { }
 
         }
-
-        private void _txtTimmaso_TextChanged(object sender, EventArgs e)
+        private void _grdDichVu_SelectionChanged(object sender, EventArgs e)
+        {
+            int pos = _grdDichVu.CurrentCell.RowIndex;
+            XemChiTietDichVu(pos);
+        }
+        private void _btnHuy_Click(object sender, EventArgs e)
+        {
+            _grdDichVu_SelectionChanged(sender, e);
+            // trạng thái button
+            add = false;
+            sua = false;
+            _btnHuy.Enabled = false;
+            _btnLuu.Enabled = false;
+            _btnThem.Enabled = true;
+            _btnSua.Enabled = true;
+            // trạng thái dữ liệu
+            _txtTendichvu.Enabled = false;
+            _tbxMota.Enabled = false;
+            _chkTrangthai.Enabled = false;
+            _txtDongia.Enabled = false;
+            _cboNhomdichvu.Enabled = false;
+        }
+        private void _TimTheoMa_TextChanged(object sender, EventArgs e)
         {
             if (_txtTimmaso.Text == "")
             {
@@ -39,117 +217,7 @@ namespace GUI
             }
         }
 
-        private void pnlArea2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void gpThongtinchitiet_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void _btnSua_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void _btnLuu_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void _btnThem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBoxX5_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void labelX7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBoxX4_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void labelX6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBoxEx2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBoxX1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBoxX3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBoxX1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void labelX4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void labelX2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblMaso_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblDatetime_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblThanhpho_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void _grdDichVu_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void _lblTimmaso_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panelEx1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void labelX1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void _txtTimkiem_TextChanged(object sender, EventArgs e)
+        private void _TimTheoTen_TextChanged_1(object sender, EventArgs e)
         {
             if (_txtTimkiem.Text == "")
             {
@@ -160,26 +228,5 @@ namespace GUI
                 _grdDichVu.DataSource = BL.DichVu_BL.TimKiemTheoTen(_txtTimkiem.Text);
             }
         }
-
-        private void _lblTimkiem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
-        {
-
-        }
-
-        private void tabControlPanel1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void _btnHuy_Click(object sender, EventArgs e)
-        {
-
-        }
-
     }
 }
