bc9c9427d4fbea610f33e6dd024c714b4e116f0f
hungb92@gmail.com <hungb92@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-09 09:49:59 +0000

    Hoan thanh ChiTietPhongBan_BL va DanhSachBan_BL

---
+++ trunk/ABC-Hospital/BL/DanhSachBan_BL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+using DA;
+
+namespace BL
+{
+    public class DanhSachBan_BL
+    {
+        // Hàm lấy chi tiết
+        public static List<DanhSachBan_DO> LayDanhSachBan(string mactpb)
+        {
+            return DA.DanhSachBan_DA.LayDanhSachBan(mactpb);
+        }
+        // Hàm thêm
+        public static void ThemBan(string maban, string mactpb, string mota, bool trangthai)
+        {
+            DA.DanhSachBan_DA.ThemBan(maban, mactpb, mota, trangthai);
+        }
+        // Hàm cập nhật
+        public static void CapNhatBan(string maban, string mota, bool trangthai)
+        {
+            DA.DanhSachBan_DA.CapNhatBan(maban, mota, trangthai);
+        }
+        // Hàm lấy mã lớn nhất
+        public static String MaBanLonNhat()
+        {
+            return DA.DanhSachBan_DA.MaBanLonNhat();
+        }
+    }
+}
