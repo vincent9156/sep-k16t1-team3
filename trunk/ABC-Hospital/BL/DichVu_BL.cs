04570576043c6700d4e7176ed057db35bceabc73
hungb92@gmail.com <hungb92@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-09 09:57:44 +0000

    Thay đổi bởi Minh Hòa

--- trunk/ABC-Hospital/BL/DichVu_BL.cs
+++ trunk/ABC-Hospital/BL/DichVu_BL.cs
@@ -1,39 +1,42 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using DA;
 using DO;
+using DA;
 namespace BL
 {
     public class DichVu_BL
     {
-            // Hàm load danh sáhc dịch vụ
+        // Hàm load danh sách dịch vụ từ database
         public static List<DichVu_DO> LoadDanhSachDichVu()
         {
             return DA.DichVu_DA.LoadDanhSachDichVu();
         }
-            // Hàm tìm kiếm theo theo mã dịch vụ
-        public static List<DichVu_DO> TimKiemTheoMa(string madichvu)
+        // Hàm thêm danh sách
+        public static void ThemDichVu(string madichvu, string manhomdichvu, string tendichvu, string dongia, string mota, bool trangthai)
         {
-            return DA.DichVu_DA.TimKiemTheoMa(madichvu);
+            DA.DichVu_DA.ThemDichVu(madichvu, manhomdichvu, tendichvu, dongia, mota, trangthai);
         }
-            // Hàm tìm kiếm theo tên dịch vụ
-        public static List<DichVu_DO> TimKiemTheoTen(string tenloai)
+        // Hàm cập nhật danh sách
+        public static void CapNhatDichVu(string madichvu, string manhomdichvu, string tendichvu, string dongia, string mota, bool trangthai)
         {
-            return DA.DichVu_DA.TimKiemTheoTen(tenloai);
+            DA.DichVu_DA.CapNhatDichVu(madichvu, manhomdichvu, tendichvu, dongia, mota, trangthai);
         }
-            // Hàm thêm mới một dịch vụ
-        public static void ThemDichVu(string maso, string manhomdichvu, string tendichvu, int dongia, string mota, bool trangthai)
+        // Hàm lấy danh sách lớn nhất
+        public static string LayMaDichVuLonNhat()
         {
-            DA.DichVu_DA.ThemDichVu(maso, manhomdichvu, tendichvu, dongia, mota, trangthai);
-            
+            return DA.DichVu_DA.LayMaDichVuLonNhat();
         }
-            // Hàm cập nhật dịch vụ
-        public static void CapNhatDichVu(string maso, string manhomdichvu, string tendichvu, int dongia, string mota, bool trangthai)
+        // Hàm tìm kiếm dịch vụ theo mã
+        public static List<DichVu_DO> TimKiemTheoMa(string maso)
         {
-            DA.DichVu_DA.CapNhatDichVu(maso, manhomdichvu, tendichvu, dongia, mota, trangthai);
-
+            return DA.DichVu_DA.TimKiemTheoMa(maso);
+        }
+        // Hàm tìm kiếm dịch vụ theo tên
+        public static List<DichVu_DO> TimKiemTheoTen(string tenphong)
+        {
+            return DA.DichVu_DA.TimKiemTheoTen(tenphong);
         }
     }
 }
