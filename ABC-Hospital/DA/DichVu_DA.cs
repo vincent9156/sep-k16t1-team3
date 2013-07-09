1e269d17a4e0d7b7b0e0c15e8ef871c21f2419dc
hungb92@gmail.com <hungb92@gmail.com@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-09 09:58:52 +0000

    Thay đổi bởi Minh Hòa

--- ABC-Hospital/DA/DichVu_DA.cs
+++ ABC-Hospital/DA/DichVu_DA.cs
@@ -1,15 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DO;
-
 namespace DA
 {
     public class DichVu_DA
     {
-        /// <summary>
-        /// Hàm lấy danh sách dịch vụ từ Database
+         /// <summary>
+        /// Hàm load danh sách dịch vụ từ Database
         /// </summary>
         /// <returns></returns>
         public static List<DichVu_DO> LoadDanhSachDichVu()
@@ -20,84 +19,24 @@ namespace DA
                 var query = from u in mh.DichVus select u;
                 foreach (var row in query)
                 {
-                DichVu_DO us = new DichVu_DO();
-                us._MaDichVu = row.MaDichVu;
-                us._MaNhomDichVu = row.MaNhomDichVu;
-                us._TenDichVu = row.TenDichVu;
-                us._DonGia = row.DonGia;
-                us._MoTa = row.MoTa;
-                us._TrangThai = row.TrangThai;
-                dsDichVu.Add(us);
-                }
-            }
-        return dsDichVu;
-        }
-        /// <summary>
-        /// Tìm kiếm theo mã dịch vụ
-        /// </summary>
-        /// <returns></returns>
-        public static List<DichVu_DO> TimKiemTheoMa(string madichvu)
-        {
-            List<DichVu_DO> dsDichVu = new List<DichVu_DO>();
-            using (Entities mh = new Entities())
-            {
-                var query = from u in mh.DichVus
-                            where u.MaDichVu.ToLower().Contains(madichvu.ToLower())
-                            select u;
-                foreach (var row in query)
-                {
                     DichVu_DO us = new DichVu_DO();
                     us._MaDichVu = row.MaDichVu;
-                    us._MaNhomDichVu = row.MaNhomDichVu;
+                    us._NhomDichVu = DA.NhomDichVu_DA.TimKiemTheoMaLayTen(row.MaNhomDichVu);
                     us._TenDichVu = row.TenDichVu;
-                    us._DonGia = row.DonGia;
                     us._MoTa = row.MoTa;
-                    us._TrangThai = row.TrangThai;
-                    dsDichVu.Add(us);
-                }
-             }
-            return dsDichVu;
-        }
-        /// <summary>
-        /// Tìm kiếm theo tên dịch vụ
-        /// </summary>
-        /// <returns></returns>
-        public static List<DichVu_DO> TimKiemTheoTen(string tenloai)
-        {
-            List<DichVu_DO> dsDichVu = new List<DichVu_DO>();
-            using (Entities mh = new Entities())
-            {
-                var query = from u in mh.DichVus
-                            where u.TenDichVu.ToLower().Contains(tenloai.ToLower())
-                            select u;
-                foreach (var row in query)
-                {
-                    DichVu_DO us = new DichVu_DO();
-                    us._MaDichVu = row.MaDichVu;
-                    us._MaNhomDichVu = row.MaNhomDichVu;
-                    us._TenDichVu = row.TenDichVu;
                     us._DonGia = row.DonGia;
-                    us._MoTa = row.MoTa;
                     us._TrangThai = row.TrangThai;
                     dsDichVu.Add(us);
                 }
             }
             return dsDichVu;
         }
-        /// <summary>
-        /// Cập nhật dịch vụ
-        /// </summary>
-        /// <param name="madichvu"></param>
-        /// <param name="manhomdicvu"></param>
-        /// <param name="tendichvu"></param>
-        /// <param name="dongia"></param>
-        /// <param name="mota"></param>
-        /// <param name="trangthai"></param>
-        public static void CapNhatDichVu(string madichvu, string manhomdichvu, string tendichvu, int dongia, string mota, bool trangthai)
+        /// Cập nhật dịch vụ 
+        public static void CapNhatDichVu(string madichvu, string manhomdichvu, string tendichvu, string dongia, string mota, bool trangthai)
         {
             using (Entities mh = new Entities())
             {
-                 var query = (from u in mh.DichVus
+                var query = (from u in mh.DichVus
                              where u.MaDichVu == madichvu
                              select u).First();
                 query.MaDichVu = madichvu;
@@ -107,14 +46,11 @@ namespace DA
                 query.MoTa = mota;
                 query.TrangThai = trangthai;
 
-                mh.SaveChanges();                             
+                mh.SaveChanges();
             }
         }
-        /// <summary>
-        /// Hàm thêm loại phòng ban
-        /// </summary>
-        /// <returns></returns>   
-        public static void ThemDichVu(string madichvu, string manhomdichvu, string tendichvu, int dongia, string mota, bool trangthai)
+        /// Hàm thêm phòng ban
+        public static void ThemDichVu(string madichvu, string manhomdichvu, string tendichvu, string dongia, string mota, bool trangthai)
         {
             using (Entities mh = new Entities())
             {
@@ -129,6 +65,70 @@ namespace DA
                 mh.AddToDichVus(us);
                 mh.SaveChanges();
             }
+        }
+        // Hàm lấy mã dịch vụ lớn nhất
+        public static String LayMaDichVuLonNhat()
+        {
+            string maDichVu;
+            using (Entities mh = new Entities())
+            {
+                var query = from u in mh.DichVus select u;
+                DichVu_DO us = new DichVu_DO();
+                foreach (var row in query)
+                {
+                    us._MaDichVu = row.MaDichVu;
+                }
+                maDichVu = us._MaDichVu.ToString();
+            }
+            return maDichVu;
+        }
+        /// <summary>
+        /// Tìm kiếm theo mã dịch vụ
+        public static List<DichVu_DO> TimKiemTheoMa(string maso)
+        {
+            List<DichVu_DO> dsDichVu = new List<DichVu_DO>();
+            using (Entities mh = new Entities())
+            {
+                var query = from u in mh.DichVus
+                            where u.MaDichVu.ToLower().Contains(maso.ToLower())
+                            select u;
+                foreach (var row in query)
+                {
+                    DichVu_DO us = new DichVu_DO();
+                    us._MaDichVu = row.MaDichVu;
+                    us._NhomDichVu = DA.NhomDichVu_DA.TimKiemTheoMaLayTen(row.MaNhomDichVu);
+                    us._TenDichVu = row.TenDichVu;
+                    us._MoTa = row.MoTa;
+                    us._DonGia = row.DonGia;
+                    us._TrangThai = row.TrangThai;
+                    dsDichVu.Add(us);
+                }
+                return dsDichVu;
+            }
+        }
+        /// <summary>
+        /// Tìm kiếm theo tên dịch vụ
+        public static List<DichVu_DO> TimKiemTheoTen(string tendichvu)
+        {
+            List<DichVu_DO> dsDichVu = new List<DichVu_DO>();
+            using (Entities mh = new Entities())
+            {
+                var query = from u in mh.DichVus
+                            where u.TenDichVu.ToLower().Contains(tendichvu.ToLower())
+                            select u;
+                foreach (var row in query)
+                {
+                    DichVu_DO us = new DichVu_DO();
+                    us._MaDichVu = row.MaDichVu;
+                    us._NhomDichVu = DA.NhomDichVu_DA.TimKiemTheoMaLayTen(row.MaNhomDichVu);
+                    us._TenDichVu = row.TenDichVu;
+                    us._MoTa = row.MoTa;
+                    us._DonGia = row.DonGia;
+                    us._TrangThai = row.TrangThai;
+                    dsDichVu.Add(us);
+                }
+            }
+            return dsDichVu;
         }
     }
 }
