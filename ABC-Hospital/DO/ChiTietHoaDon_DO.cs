0a596ddb6efd64fe026e80a7cf2d230a0d714fac
khanghuynh92 <khanghuynh92@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-12 05:28:57 +0000

    Thêm các data object BenhNan, ChitietHoadon, Hoadon

---
+++ ABC-Hospital/DO/ChiTietHoaDon_DO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+    public class ChiTietHoaDon_DO
+    {
+        public string _MaCTHD { get; set; }
+        public string _MaHoaDon { get; set; }
+        public string _MaDichVu { get; set; }
+        public string _TenBenhNhan { get; set; }
+    }
+}
---
+++ ABC-Hospital/DO/HoaDon_DO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+    public class HoaDon_DO
+    {
+        public string _MaHoaDon { get; set; }
+        public string _MaBenhNhan { get; set; }
+        public string _MaPhongBan { get; set; }
+        public string _MaNguoiNhap { get; set; }
+        public string _MaNguoiThu { get; set; }
+        public string _GioiTinh { get; set; }
+        public int? _Tuoi { get; set; }
+        public int? _TongTien { get; set; }
+        public DateTime _NgayThang { get; set; }
+        public string _TrangThai { get; set; }
+    }
+}
