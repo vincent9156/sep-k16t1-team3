a028352c88a3126b5c6e5abb4918caf4ce070a98
khanghuynh92 <khanghuynh92@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-15 13:58:52 +0000

    Update HoaDon PhanCong_DO

---
+++ ABC-Hospital/DO/PhanCong_DO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+    public class PhanCong_DO
+    {
+        public string _MaNguoiDung { get; set; }
+        public int? _VaiTro { get; set; }
+        public string _PhongBan { get; set; }
+        public string _Ban { get; set; }
+        public bool? _TrangThai { get; set; }
+    }
+}
