465d355becd8fb6d9bb2c20a3e41182387d9c116
khanghuynh92 <khanghuynh92@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-15 13:57:38 +0000

    Update HoaDon PhanCong, DanhSachBan cho chức năng phân quyền cho cashier

---
+++ trunk/ABC-Hospital/DA/HoaDon_DA.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+
+namespace DA
+{
+    public class HoaDon_DA
+    {
+        // Hàm lưu hóa đơn 
+        public static void ThemHoaDon(string mabenhnhan, string maphongban, string manguoinhap, string manguoithu, string gioitinh, int tuoi, int tongtien,string ngaythang, bool trangthai)
+        {
+            using (Entities sh = new Entities())
+            {
+                //HoaDon us = new HoaDon();
+
+                //sh.ExecuteStoreCommand("EXEC sp_TangMa '"+mabenhnhan+"','"+maphongban+"','"+manguoinhap+"','"+manguoithu+"','"+gioitinh+"',"+tuoi+","+tongtien+","+trangthai+"");
+                sh.SP_TangMa(mabenhnhan, maphongban, manguoinhap, manguoithu, gioitinh, tuoi, tongtien, ngaythang, trangthai);
+                //sh.ExecuteStoreCommand("EXEC sp_TangMa 'A130000001','TN','AD00001','AD00002','NU',19,200000,TRUE");
+                sh.SaveChanges();
+            }
+        }
+        // Hàm lấy mã hóa đơn theo benh nhan va phong ban
+        public static String LayMaHD(string mabenhnhan, string mapb)
+        {
+            string maHD;
+            using (Entities sh = new Entities())
+            {
+                var query = from u in sh.HoaDons
+                            where u.MaBenhNhan == mabenhnhan
+                                && u.MaPhongBan == mapb
+                            select u;
+                HoaDon_DO us = new HoaDon_DO();
+                foreach (var row in query)
+                {
+                    us._MaHoaDon = row.MaHoaDon;
+                }
+                maHD = us._MaHoaDon.ToString();
+            }
+            return maHD;
+        }
+        // lây danh sách bênh nhân chờ xác nhận hóa đơn
+        /*public static List<HoaDon_DO> DSBenhNhanCho(string manguoinhap, DateTime ngaythang, bool trangthai)
+        {
+            List<HoaDon_DO> dsBenhNhanCho = new List<HoaDon_DO>();
+            using (Entities sh = new Entities())
+            {
+                var query = from u in sh.HoaDons
+                            where u.MaNguoiNhap == manguoinhap
+                            && u.NgayThang.ToString().Contains(ngaythang.ToShortDateString())
+                            && u.TrangThai == trangthai
+                            select u;
+                foreach (var row in query)
+                {
+                    HoaDon_DO us = new HoaDon_DO();
+                    us._MaBenhNhan = row.MaBenhNhan;
+                    us._TenBenhNhan = DA.BenhNhan_DA.LayTenBenhNhan(row.MaBenhNhan);
+
+                    dsBenhNhanCho.Add(us);
+                }
+                return dsBenhNhanCho;
+            }*/
+    }
+}
+
---
+++ trunk/ABC-Hospital/DA/PhanCong_DA.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+namespace DA
+{
+    public class PhanCong_DA
+    {
+        // Hàm lấy thông tin từ csdl lên c#
+        public static List<PhanCong_DO> LayPhanCong()
+        {
+            List<PhanCong_DO> dsPhancong = new List<PhanCong_DO>();
+            
+            using (Entities tk = new Entities())
+            {
+                var query = from u in tk.PhanCongs
+                            select new { u.MaNguoiDung, u.VaiTro, u.PhongBan, u.Ban, u.TrangThai };
+                foreach(var row in query)
+                {
+                    PhanCong_DO us = new PhanCong_DO();
+                    us._MaNguoiDung = row.MaNguoiDung;
+                    us._VaiTro = row.VaiTro;
+                    us._PhongBan = row.PhongBan;
+                    us._Ban = row.Ban;
+                    us._TrangThai = row.TrangThai;
+
+                    dsPhancong.Add(us);
+                }
+            }
+            return dsPhancong;
+
+        }
+        // Hàm thêm người dùng vào bảng phân công
+        public static void ThemPhanCong(string manguoidung, int vaitro, string phongban, string ban, bool trangthaidn)
+        {
+            using (Entities tk = new Entities())
+            {
+                PhanCong pc = new PhanCong();
+                pc.MaNguoiDung = manguoidung;
+                pc.VaiTro = vaitro;
+                pc.PhongBan = phongban;
+                pc.Ban = ban;
+                pc.TrangThai = trangthaidn;
+
+                tk.AddToPhanCongs(pc);
+                tk.SaveChanges();
+
+            }
+        }
+        // Hàm cập nhật người dùng trong bảng phân công
+        public static void CapNhatPhanCong(string manguoidung, int vaitro, string phongban, string ban, bool trangthai)
+        {
+            using (Entities tk = new Entities())
+            {
+                var query = (from u in tk.PhanCongs
+                            where u.MaNguoiDung == manguoidung
+                            select u).First();
+                query.MaNguoiDung = manguoidung;
+                query.VaiTro = vaitro;
+                query.PhongBan = phongban;
+                query.Ban = ban;
+                query.TrangThai = trangthai;
+
+                tk.SaveChanges();
+            }
+        }
+        
+
+        // Hàm lọc người dùng đã đăng nhập tại phòng nào bàn nào
+        public static List<PhanCong_DO> LocNguoiDung(string phong, string ban, bool trangthai)
+        {
+            List<PhanCong_DO> dsPhancong = new List<PhanCong_DO>();
+            using (Entities tk = new Entities())
+            {
+                var query = from u in tk.PhanCongs
+                            where u.PhongBan == phong &&
+                                    u.Ban == ban &&
+                                    u.TrangThai == trangthai
+                            select u;
+                foreach (var row in query)
+                {
+                    PhanCong_DO us = new PhanCong_DO();
+                    us._MaNguoiDung = row.MaNguoiDung;
+                    us._VaiTro = row.VaiTro;
+                    us._PhongBan = row.PhongBan;
+                    us._Ban = row.Ban;
+                    us._TrangThai = row.TrangThai;
+
+                    dsPhancong.Add(us);
+                }                     
+            }
+            return dsPhancong;
+        }
+    }
+}
