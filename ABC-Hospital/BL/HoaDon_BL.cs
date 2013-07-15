c41344e9241420329aa1972db8563076938758b0
khanghuynh92 <khanghuynh92@2ddd8bba-66c4-09f5-5937-053d67c2850f>
2013-07-15 13:53:47 +0000

    Update 2 file HoaDon vs PHanCong cho phan Xet quyen cho thu Ngan

---
+++ ABC-Hospital/BL/HoaDon_BL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+using DA;
+namespace BL
+{
+    public class HoaDon_BL
+    {
+        // Hàm lưu hóa đơn
+        public static void ThemHoaDon(string mabenhnhan, string maphongban, string manguoinhap, string manguoithu, string gioitinh, int tuoi, int tongtien, string ngaythang, bool trangthai)
+        {
+            DA.HoaDon_DA.ThemHoaDon(mabenhnhan, maphongban, manguoinhap, manguoithu, gioitinh, tuoi, tongtien, ngaythang, trangthai);
+        }
+        // Hàm lấy mã hd theo nhóm dv
+        public static String LayMaHDTheoNhomDV(string mabenhnan, string mapb)
+        {
+            return DA.HoaDon_DA.LayMaHD(mabenhnan, mapb);
+        }
+        // Hàm lấy ds bệnh nhân chờ
+        /*public static List<HoaDon_DO> LayDSBenhNhanCho(string manguoinhap, DateTime ngaythang, bool trangthai)
+        {
+            //return DA.HoaDon_DA.DSBenhNhanCho(manguoinhap, ngaythang, trangthai);
+        }*/
+    }
+}
---
+++ ABC-Hospital/BL/PhanCong_BL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DA;
+using DO;
+namespace BL
+{
+    public class PhanCong_BL
+    {
+        public static List<PhanCong_DO> LayPhanCong()
+        {
+            return DA.PhanCong_DA.LayPhanCong();
+        }
+        // Hàm thêm phân công
+        public static void ThemPhanCong(string manguoidung, int vaitro, string phongban, string ban, bool trangthai)
+        {
+            DA.PhanCong_DA.ThemPhanCong(manguoidung, vaitro, phongban, ban, trangthai);
+        }
+        // Hàm update phân công
+        public static void CapNhatPhanCong(string manguoidung, int vaitro, string phongban, string ban, bool trangthai)
+        {
+            DA.PhanCong_DA.CapNhatPhanCong(manguoidung, vaitro, phongban, ban, trangthai);
+        }
+        // Hàm kiểm tra người dùng đã có trong bản phân công chưa
+        public static bool KiemTraNguoiDungDaPhanCong(string manguoidung)
+        {
+            List<PhanCong_DO> us = LayPhanCong();
+            foreach (PhanCong_DO row in us)
+            {
+                if (manguoidung == row._MaNguoiDung) { return true; }
+            }
+            return false;
+        }
+        // Hàm lọc người dùng đã đăng nhập theo phòng và bàn
+        public static List<PhanCong_DO> LocNguoiDung(string phong, string ban, bool trangthaidn)
+        {
+            return DA.PhanCong_DA.LocNguoiDung(phong, ban, trangthaidn);
+        }
+    }
+}
